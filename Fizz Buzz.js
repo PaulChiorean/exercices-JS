3d0b55909df35c6a356b4767512bc8f9aa8ebd7c
Chiorean Paul <paul.chiorean08@gmail.com>
2018-02-25 12:33:07 +0200

    Update Fizz Buzz.js

--- Fizz Buzz.js
+++ Fizz Buzz.js
@@ -15,3 +15,17 @@ for (var i=1;i<=n;i++) {
 }
   return newArray;
 }
+
+// Return an string
+function fizzbuzz(n){
+    for (var i = 0; i<=n; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");}
+        else if(i % 3 === 0){
+            console.log("Fizz");}
+        else if(i % 5 ===0){
+            console.log("Buzz");}
+        else{
+            console.log(i);}
+    }
+}
