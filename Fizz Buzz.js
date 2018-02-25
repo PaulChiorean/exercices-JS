8ecf3634aca8acc3882c651921400594a7c2dc2a
Chiorean Paul <paul.chiorean08@gmail.com>
2018-02-25 11:33:42 +0200

    Add files via upload

---
+++ Fizz Buzz.js
@@ -0,0 +1,17 @@
+// Return an array
+function fizzbuzz(n)
+{
+var newArray=[];
+for (var i=1;i<=n;i++) {
+    if (i%15==0) {
+      newArray.push('FizzBuzz');
+    } else if (i%3==0){
+      newArray.push('Fizz');
+    } else if (i%5==0){
+      newArray.push('Buzz');
+    } else {
+      newArray.push(i);
+    }
+}
+  return newArray;
+}
